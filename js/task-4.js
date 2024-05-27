0487223f1165f142cec93950a0ce2585dbe110cb
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2024-05-27 16:45:17 +0300

    all task

---
+++ js/task-4.js
@@ -0,0 +1,50 @@
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+
+
+const getTotalBalanceByGender = (users, gender) => {
+    return users
+    .filter(user => gender === user.gender)
+    .reduce((acc, userBalance) => acc += userBalance.balance, 0)
+    
+}
+
+    
+console.log(getTotalBalanceByGender(clients, "male"));
+console.log(getTotalBalanceByGender(clients, "female")); 
